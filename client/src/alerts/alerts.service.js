41dd5b33e833cd33149a3452846b7b513ae81f5f
AkshayKS02 <aks969875@gmail.com>
2026-01-04 02:07:51 +0530

    Changed some UI Designs

--- client/src/alerts/alerts.service.js
+++ client/src/alerts/alerts.service.js
@@ -1,5 +1,5 @@
 const LAST_ALERT_CHECK = "lastAlertCheck";
-const ALERT_INTERVAL =  60 * 1000; // 5 hours
+const ALERT_INTERVAL =  60*60 * 1000; // 5 hours
 
 export function shouldCheckAlerts() {
   const last = localStorage.getItem(LAST_ALERT_CHECK);
